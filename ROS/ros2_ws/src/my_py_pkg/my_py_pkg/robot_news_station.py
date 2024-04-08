e532f78dcfe6be97a844f9e05872531892bb16e3
Orlando Espinosa <orlandojespinosa@gmail.com>
2024-04-08 09:17:40 -0500

    refactor(my_py_pkg): change publisher robot name

--- ROS/ros2_ws/src/my_py_pkg/my_py_pkg/robot_news_station.py
+++ ROS/ros2_ws/src/my_py_pkg/my_py_pkg/robot_news_station.py
@@ -9,7 +9,7 @@ class RobotNewsStationNode(Node):
     def __init__(self):
         super().__init__("robot_news_station")
 
-        self.robot_name = 'R2D2'
+        self.robot_name = 'PyRobot'
         self.publisher_ = self.create_publisher(String, "robot_news", 10)
         self.timer_ = self.create_timer(0.5, self.publish_news)
         self.get_logger().info("Robot News Station has been started.")
